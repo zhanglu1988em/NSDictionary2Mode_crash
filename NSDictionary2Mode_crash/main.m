af2f60cdc8ae0523007e29d15968b4cce7c8abd4
zhanglu1988em <zhanglu1988em@163.com>
2015-12-30 17:17:40 +0800

    Initial Commit

---
+++ NSDictionary2Mode_crash/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSDictionary2Mode_crash
+//
+//  Created by wbder on 15/12/30.
+//  Copyright © 2015年 wbder. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
